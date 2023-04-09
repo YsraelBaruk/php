886c2cff0c38959a1225d46cf6c641eb88672d67
Israel Baruque <israekbaruque@gmail.com>
2023-04-09 18:15:19 -0300

    insert usuario

--- model/Usuario.php
+++ model/Usuario.php
@@ -48,7 +48,7 @@
             $this->foto = $foto;
         }
 
-        public function getTelefone() {
+        public function getTel() {
             return $this->tel;
         }
 
@@ -86,6 +86,33 @@
 
         public function setModificationTime($modification_time) {
             $this->modification_time = $modification_time;
+        }
+
+        public function cadastrar(){
+            $conexao = (new DataBase())->connection();
+            $query = "INSERT INTO usuarios (email, senha, nome, foto, tel, endereco, cpf, creation_time, modification_time)
+            VALUES (:email, :senha, :nome, :foto, :tel, :endereco, :cpf, :creation_time, :modification_time)";
+
+            $stmt = $conexao->prepare($query);
+            $stmt->bindValue(':email', $this->getEmail());
+            $stmt->bindValue(':senha', $this->getSenha());
+            $stmt->bindValue(':nome', $this->getNome());
+            $stmt->bindValue(':foto', $this->getFoto());
+            $stmt->bindValue(':tel', $this->getTel());
+            $stmt->bindValue(':endereco', $this->getEndereco());
+            $stmt->bindValue(':cpf', $this->getCpf());
+            $stmt->bindValue(':creation_time', $this->getCreationTime());
+            $stmt->bindValue(':modification_time', $this->getModificationTime());
+
+            $resultado = $stmt->execute();
+
+            if($resultado){
+                $this->setId($conexao->lastInsertId());
+                return true;
+            }
+            else {
+                return false;
+            }
         }
 
         
